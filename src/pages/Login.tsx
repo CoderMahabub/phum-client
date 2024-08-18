ea8025f5390e6736d597899fe92ab41752ce99fd
CoderMahabub <CoderMahabub@gmail.com>
2024-08-19 08:48:38 +0900

    Basic Login Form

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,37 @@
+import { Button } from "antd";
+import { useForm } from "react-hook-form";
+import { useLoginMutation } from "../redux/features/auth/authApi";
+
 const Login = () => {
+  const { register, handleSubmit } = useForm({
+    defaultValues: {
+      userId: "A-0001",
+      password: "admin123",
+    },
+  });
+  const [login, { data, error }] = useLoginMutation();
+  console.log("data=>", data);
+  console.log("error=>", error);
+
+  const onSubmit = (data) => {
+    const userInfo = {
+      id: data.userId,
+      password: data.password,
+    };
+    login(userInfo);
+  };
   return (
-    <div>
-      <h1>Login Component</h1>
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <label htmlFor="id">ID:</label>
+        <input type="text" id="id" {...register("userId")} />
+      </div>
+      <div>
+        <label htmlFor="password">PASSWORD:</label>
+        <input type="password" id="password" {...register("password")} />
+      </div>
+      <Button htmlType="submit">login</Button>
+    </form>
   );
 };
 
