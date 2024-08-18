23db830d3235874724ed232ca15425730e2c96e4
CoderMahabub <CoderMahabub@gmail.com>
2024-08-19 08:19:08 +0900

    Create authAlice and authApi by injecting baseApi

---
+++ src/redux/features/auth/authApi.ts
@@ -0,0 +1,18 @@
+import { baseApi } from "../../api/baseApi";
+
+const authApi = baseApi.injectEndpoints({
+  endpoints: (builder) => ({
+    login: builder.mutation({
+      query: (userInfo) => ({
+        url: "/auth/login",
+        method: "POST",
+        body: userInfo,
+      }),
+    }),
+    // logout:builder.mutation({
+    //     query
+    // })
+  }),
+});
+
+export const { useLoginMutation } = authApi;
