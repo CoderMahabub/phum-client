74b844fa1636523fad6c4a510db2a02546bf47e4
CoderMahabub <CoderMahabub@gmail.com>
2024-08-19 09:55:05 +0900

    Decode Token And Set User In Local State

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,24 +1,29 @@
 import { Button } from "antd";
 import { useForm } from "react-hook-form";
 import { useLoginMutation } from "../redux/features/auth/authApi";
+import { useDispatch } from "react-redux";
+import { setUser } from "../redux/features/auth/authSlice";
+import { verifyToken } from "../utils/verifyToken";
 
 const Login = () => {
+  const dispatch = useDispatch();
   const { register, handleSubmit } = useForm({
     defaultValues: {
       userId: "A-0001",
       password: "admin123",
     },
   });
-  const [login, { data, error }] = useLoginMutation();
-  console.log("data=>", data);
-  console.log("error=>", error);
+  const [login, { error }] = useLoginMutation();
 
-  const onSubmit = (data) => {
+  const onSubmit = async (data) => {
     const userInfo = {
       id: data.userId,
       password: data.password,
     };
-    login(userInfo);
+    const res = await login(userInfo).unwrap();
+    const user = verifyToken(res.data.accessToken);
+    console.log(user);
+    dispatch(setUser({ user: user, token: res.data.accessToken }));
   };
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
