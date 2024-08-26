6393034be2db387bc7c46605f414316efaef429c
CoderMahabub <CoderMahabub@gmail.com>
2024-08-27 01:18:02 +0900

    Handle form default values and resolve type errors

--- src/components/forms/PHInput.tsx
+++ src/components/forms/PHInput.tsx
@@ -1,7 +1,12 @@
 import { Input } from "antd";
 import { Controller } from "react-hook-form";
+type TInputProps = {
+  type: string;
+  name: string;
+  label?: string;
+};
 
-const PHInput = ({ type, name, label }) => {
+const PHInput = ({ type, name, label }: TInputProps) => {
   return (
     <div style={{ marginBottom: "20px" }}>
       {label ? label : null}
